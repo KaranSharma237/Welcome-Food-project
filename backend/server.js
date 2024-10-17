0b9390870b7584a822a01ac3ad083905ef31afef
Karan <ks3979280@gmail.com>
2024-10-17 15:24:39 +0530

    make some changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
 import app from "./app.js";
-const cors = require('cors');
-app.use(cors());
 app.listen(process.env.PORT, () => {
     console.log(`Server Running On Port ${process.env.port}`);
 })
