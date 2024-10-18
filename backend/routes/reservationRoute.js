ba775c4c9b1d6f911d36862ae8e41a22313f8a93
KaranSharma237 <ks3979280@gmail.com>
2024-10-18 17:33:23 +0530

    Update reservationRoute.js

--- backend/routes/reservationRoute.js
+++ backend/routes/reservationRoute.js
@@ -1,10 +1,9 @@
-
 import express from "express";
 import { sendReservation } from "../controller/reservation.js";
 
 const router = express.Router();
 
-// POST endpoint for creating a reservation at /send
-router.post("/send", sendReservation); // Now it handles POST requests to /api/v1/reservation/send
+// POST endpoint for creating a reservation directly at /api/v1/reservation
+router.post("/", sendReservation);
 
 export default router;
